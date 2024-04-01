9e4ac233d1af52ad8bed06423fb96379fde6d4e6
njbunge02 <njbunge02@gmail.com>
2024-04-01 14:42:27 -0400

    merge

--- obj/Debug/net8.0/TigerTix.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/TigerTix.Web.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TigerTix.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7541cd02d747aa38776dfdbc41c58d7c50c82a90")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0987553e3c9bedd06f73038e92223a2c27519bc")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("TigerTix.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TigerTix.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
