bcaa61b7564ce1ff04b718fd873651277f4b0d5d
Noah Fultz <143000565+nfultz23@users.noreply.github.com>
2024-04-05 09:35:24 -0400

    Added Documentation to User.cs
    Added short descriptions of private fields

--- Data/Entities/User.cs
+++ Data/Entities/User.cs
@@ -2,10 +2,9 @@ namespace TigerTix.Web.Data.Entities
 {
     public class User
     {
-        public int Id {get;set;}
-        public string userName {get;set;}
-        public string firstName {get;set;}
-        public string lastName {get;set;}
-        
+        public int Id {get;set;}           //The User's unique ID number
+        public string userName {get;set;}  //The display name of the user
+        public string firstName {get;set;} //User's first name
+        public string lastName {get;set;}  //User's last name
     }
 }
