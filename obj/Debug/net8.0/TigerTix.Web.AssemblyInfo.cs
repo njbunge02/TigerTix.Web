03fc5076c8514887d359b289e4c8961a6a9f0e27
Noah Fultz <noah.fultz314@gmail.com>
2024-04-09 17:17:38 -0400

    Page formatting on CheckEvent.cshtml
    Changed values from percentages to pixels to make the site look nice with different window resolutions.

--- obj/Debug/net8.0/TigerTix.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/TigerTix.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TigerTix.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8bea71d75f83ec60ad2e3e6b74341a7ffd7dfc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88e66bcf0a7645627c7d7dc6991287cb8b42cad2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TigerTix.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TigerTix.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
