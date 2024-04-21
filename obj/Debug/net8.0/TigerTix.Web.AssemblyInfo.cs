91d1f2413f0900817fe35db0c997fe1b218db115
njbunge02 <njbunge02@gmail.com>
2024-04-21 19:11:56 -0400

    Login

--- obj/Debug/net8.0/TigerTix.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/TigerTix.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TigerTix.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c3285d89d5900ed05d0b7494701a52455b70b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+003f63e7ad164be820a052b2740360dd5dbb8cb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TigerTix.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TigerTix.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
