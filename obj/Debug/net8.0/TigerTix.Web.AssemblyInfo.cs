c7f91882230cd18e2f803cd83b75553e6d93f311
njbunge02 <njbunge02@gmail.com>
2024-04-21 20:01:42 -0400

    FooterFixed

--- obj/Debug/net8.0/TigerTix.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/TigerTix.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TigerTix.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6dd538c9d984ba3096b067ac0b0abab04986e0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84fec6169e853e4d70c4a5f798cd8bea1552b65f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TigerTix.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TigerTix.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
